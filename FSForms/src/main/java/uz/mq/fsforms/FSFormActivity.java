40750cae98900f0773aa2a55e6b3be137d75f462
Muhammadqodir <m.qodir777@gmail.com>
2021-02-13 23:58:58 +0500

    Created FSFormsActivity

---
+++ FSForms/src/main/java/uz/mq/fsforms/FSFormActivity.java
@@ -0,0 +1,14 @@
+package uz.mq.fsforms;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FSFormActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fsform);
+    }
+}
